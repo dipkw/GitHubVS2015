87986c3ed8c2908ba2ea6c685968aaebbce98035
jolly <jolly@dipndipkw.com>
2016-10-16 16:52:28 +0300

    Stock Adjustment

---
+++ dipndipInventory/ViewModels/WHStockAdjustmentViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dipndipInventory.ViewModels
+{
+    public class WHStockAdjustmentViewModel
+    {
+        public int wh_item_id { get; set; }
+        public string wh_item_code { get; set; }
+        public string wh_item_description { get; set; }
+        public decimal adj_qty { get; set; }
+        public int wh_unit_id { get; set; }
+        public string wh_unit_description { get; set; }
+        public decimal qty { get; set; }
+        public int row_index { get; set; }
+    }
+}
--- dipndipInventory/Views/Stock/ckwhstockadjView.xaml.cs
+++ dipndipInventory/Views/Stock/ckwhstockadjView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using dipndipInventory.EF;
 using dipndipInventory.EF.DataServices;
 using dipndipInventory.Helpers;
+using dipndipInventory.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,6 +32,7 @@ namespace dipndipInventory.Views.Stock
         {
             InitializeComponent();
             ShowTaskBar.ShowInTaskbar(this, "Stock Adjustment");
+
             ReadAllWHItems();
         }
 
@@ -38,6 +40,58 @@ namespace dipndipInventory.Views.Stock
         {
             IEnumerable<ckwh_items> objItems = _context.ReadAllWHItems();
             dgCKWHItems.ItemsSource = objItems;
+            
+            WHItemUnitService unitContext = new WHItemUnitService();
+            //List<WHStockAdjustmentViewModel> objWHStkAdjVM = new List<WHStockAdjustmentViewModel>();
+            //foreach(var stkItem in objItems)
+            //{
+            //    WHStockAdjustmentViewModel whsav = new WHStockAdjustmentViewModel();
+            //    whsav.wh_item_id = stkItem.Id;
+            //    whsav.wh_item_code = stkItem.wh_item_code;
+            //    whsav.wh_item_description = stkItem.wh_item_description;
+            //    whsav.wh_unit_id = (int)unitContext.GetIdOfBaseUnit(stkItem.Id);
+            //    whsav.wh_unit_description = stkItem.wh_unit_description;
+            //    whsav.adj_qty = 0m;
+            //    //whsav.row_index = stkItem.;
+            //    objWHStkAdjVM.Add(whsav);
+            //}
+            ////dgCKWHItems.ItemsSource = objItems;
+            //dgCKWHItems.ItemsSource = null;
+
+            //dgCKWHItems.ItemsSource = objWHStkAdjVM.ToList();
+        }
+
+        private void FillAllUnits(int wh_item_id)
+        {
+            WHItemUnitService unitContext = new WHItemUnitService();
+            IEnumerable<wh_item_unit> objUnits = unitContext.ReadAllWHItemUnitsByWHItemId(wh_item_id);
+            cmbUnit.DisplayMemberPath = "ck_units.unit_description";
+            cmbUnit.SelectedValuePath = "ck_unit_d";
+            cmbUnit.ItemsSource = objUnits.ToList();
+            if(objUnits.Count()<1)
+            {
+                MessageBox.Show("Please assign unit to make any Stock Adustment for this item");
+            }
+        }
+
+        private void dgCKWHItems_SelectionChanged(object sender, SelectionChangeEventArgs e)
+        {
+            //WHStockAdjustmentViewModel objWHStkAdjVM = (dgCKWHItems.SelectedItem) as WHStockAdjustmentViewModel;
+
+            //if(objWHStkAdjVM.wh_unit_id == 0)
+            //{
+            //    MessageBox.Show("Please configure unit ");
+            //    return;
+            //}
+            //int wh_item_id = objWHStkAdjVM.wh_item_id;
+
+            ckwh_items objItem = (dgCKWHItems.SelectedItem) as ckwh_items;
+            int wh_item_id = objItem.Id;
+
+            FillAllUnits(wh_item_id);
+            txtItemCode.Value = objItem.wh_item_code;
+            txtDescription.Value = objItem.wh_item_description;
+            txtQty.Focus();
         }
     }
 }
